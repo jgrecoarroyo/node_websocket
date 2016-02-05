6d9d4c612489ce7db4ce885f56618ffe83d57072
Juan Greco <jgrecoarroyo@gmail.com>
2016-02-04 18:02:48 -0800

    initial commit with websockets example

---
+++ server.js
@@ -0,0 +1,51 @@
+var http = require('http');
+
+// Create simple server
+var server = http.createServer(function(request, response) {});
+server.listen(3333, function() {
+    console.log((new Date()) + ' Server is listening on port 8080');
+});
+
+// Create web socker server
+var WebSocketServer = require('websocket').server;
+wsServer = new WebSocketServer({
+    httpServer: server
+});
+
+// Variables to store connected clients
+var count = 0;
+var clients = {};
+
+// Listen for connections
+wsServer.on('request', function(r){
+
+    // Accept connection
+    var connection = r.accept('echo-protocol', r.origin);
+
+    // Specific id for this client & increment count
+    var id = count++;
+    // Store the connection method so we can loop through & contact all clients
+    clients[id] = connection
+
+    // Create event listener
+    connection.on('message', function(message) {
+
+        // The string message that was sent to us
+        var msgString = message.utf8Data;
+        console.log("client: " + id +  " sent: " + msgString);
+
+        // Loop through all clients
+        for(var i in clients){
+            // Send a message to the client with the message
+            clients[i].sendUTF(msgString);
+        }
+
+    });
+
+    // On client disconnection
+    connection.on('close', function(reasonCode, description) {
+        delete clients[id];
+        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+    });
+
+});
